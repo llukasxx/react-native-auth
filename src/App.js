23d5c534b8afacbf5245a1371c7bbb9fe0cd3956
Łukasz Gorzelany <llukasxx@gmail.com>
2017-04-30 18:12:35 +0200

    Add logging off logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 export default class App extends Component {
+  state = {
+    loggedIn: null
+  };
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyAa9-e3LsDN8k5iKIQw4CAFTkO9YnU6um8',
@@ -15,12 +18,37 @@ export default class App extends Component {
       storageBucket: 'auth-f6df4.appspot.com',
       messagingSenderId: '585151601649'
     });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log off
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
-        <Header headerText={'Authentication'} />
-        <LoginForm />
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
